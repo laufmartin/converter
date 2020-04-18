f2933a33ac23f6aa641ffedc30ab9d14fd087c49
Glitch (converter-lauf) <none>
2020-04-18 21:30:28 +0000

    🎽🏤 Updated with Glitch

---
+++ controllers/convertHandler.js
@@ -0,0 +1,55 @@
+/*
+*
+*
+*       Complete the handler logic below
+*       
+*       
+*/
+
+function ConvertHandler() {
+  
+  this.getNum = function(input) {
+    var result;
+    
+    return result;
+  };
+  
+  this.getUnit = function(input) {
+    var result;
+    
+    return result;
+  };
+  
+  this.getReturnUnit = function(initUnit) {
+    var result;
+    
+    return result;
+  };
+
+  this.spellOutUnit = function(unit) {
+    var result;
+    
+    return result;
+  };
+  
+  this.convert = function(initNum, initUnit) {
+    const galToL = 3.78541;
+    const lbsToKg = 0.453592;
+    const miToKm = 1.60934;
+    const LToGal = 1/galToL;
+    const KgToLbs = 1/lbsToKg;
+    const KmToMi = 1/miToKm;
+    var result;
+    
+    return result;
+  };
+  
+  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
+    var result;
+    
+    return result;
+  };
+  
+}
+
+module.exports = ConvertHandler;
